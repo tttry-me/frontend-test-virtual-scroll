f77f8db317114356e07524b886477e67686a1c6c
kzubkov <kirya.zubkov.98@list.ru>
2024-07-01 02:32:12 +0300

    FEAT: add loading spinner

--- src/views/DemonstratePage.tsx
+++ src/views/DemonstratePage.tsx
@@ -4,7 +4,7 @@ import {VirualScroll} from '../components/VirtualScroll.tsx/VirualScroll';
 export function DemonstratePage(): React.ReactElement {
   return (
     <div className='flex flex-col items-center justify-center w-screen min-h-screen bg-gray-900 p-10'>
-      <h1 className='text-lg text-gray-400 font-medium'>Virtual Scroll</h1>
+      <h1 className='text-lg text-gray-400 font-medium pb-3'>Virtual Scroll</h1>
       <VirualScroll maxIndex={100} minIndex={1} startIndex={6} itemHeight={56} amount={6} tolerance={3} />
     </div>
   );
